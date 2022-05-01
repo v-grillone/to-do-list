e25757d0d2c3a21b7fa027583f3dcb7449b0cacf
Vincenzo Grillone <vincenzo_1996@hotmail.com>
2022-05-01 16:09:17 -0400

    small updates and task delete

--- task-add.js
+++ task-add.js
@@ -6,14 +6,37 @@ let taskAdd = function(){
         let addInput = document.getElementById('task-add-input')
         let inputValue = addInput.value;
         // adding to dom
-
+        let tasksDiv = document.getElementById('tasks');
+        let taskDiv = document.createElement('div');
+        taskDiv.classList.add('task')
+        let taskInput = document.createElement('input');
+        taskInput.type = 'text';
+        taskInput.value = inputValue;
+        taskInput.readOnly = 'true';
+        taskInput.maxLength = '40';
+        let taskBtn = document.createElement('button');
+        let taskDelete = document.createElement('i');
+        taskDelete.classList.add('fa-solid');
+        taskDelete.classList.add('fa-trash-can');
+        taskBtn.appendChild(taskDelete);
+        taskDiv.appendChild(taskInput);
+        taskDiv.appendChild(taskBtn);
+        tasksDiv.appendChild(taskDiv);
         // reseting back to original
         let addContainer = document.getElementById('task-add-container')
         addContainer.style.display = 'none';
         addInput.value = '';
 
-        console.log(inputValue);
-    })
+
+        // to be removed after webkit initialize
+        deleteBtn = document.querySelectorAll('.fa-trash-can');
+        deleteBtn.forEach(element => {
+            element.addEventListener('click', () => {
+                let task = element.parentElement.parentElement;
+                task.remove()
+                })
+            });
+        })
 }
 
 taskAdd();
